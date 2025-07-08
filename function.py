40ad9062264572f447175e31522daaebdbe0218e
dev-code-create <ayushkankale04@gmail.com>
2025-07-09 00:15:30 +0530

    functions

---
+++ function.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello, welcome to the Python learning module!")
+
+for i in range(3):
+    greet()
+
+def add_numbers(a,b):
+    return a+b
+
+add = add_numbers(5,10)
+print(add)
--- mini_challenge2.py
+++ mini_challenge2.py
@@ -4,6 +4,8 @@ num3 = int(input("Enter the third number: "))
 
 Numbers = [num1, num2, num3]
 print(Numbers)
+
 print(Numbers[0]+Numbers[1]+Numbers[2])
+
 print(sum(Numbers))
 
