f5b7dbaa612de99e80024841c661d6e42e12e7b6
dev-code-create <ayushkankale04@gmail.com>
2025-07-08 23:52:12 +0530

    Create mini_challenge2.py

---
+++ mini_challenge2.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+Numbers = [num1, num2, num3]
+print(Numbers)
+print(Numbers[0]+Numbers[1]+Numbers[2])
+print(sum(Numbers))
+
