82d1c796ada1e4eef113fc341a3d7bb0b06a2f45
dev-code-create <ayushkankale04@gmail.com>
2025-07-09 23:48:30 +0530

    reading

--- File_hanndlng.py
+++ File_hanndlng.py
@@ -1,2 +1,10 @@
 with open("data.txt", "w") as file:
     file.write("Hello! This is my first file.")
+
+#appending text to the file
+with open("data.txt", "a") as file:
+    file.write("\nAdding more text.")
+
+with open("data.txt", "r") as file:
+    content = file.read()
+    print(content)
