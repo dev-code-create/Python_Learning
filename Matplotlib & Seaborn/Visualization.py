809c5e3b42623ef28919a46928b18ece43e9f3cb
dev-code-create <ayushkankale04@gmail.com>
2025-07-11 16:34:17 +0530

    Line chart

---
+++ Matplotlib & Seaborn/Visualization.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x=[1,2,3,4,5]
+y=[10,20,30,40,50]
+
+plt.plot(x,y)
+plt.title("Line Chart")
+plt.xlabel("Time")
+plt.ylabel("Value")
+plt.show()
