d0fb34610d418b64dd04b4fb2e4e676ac13a1ce3
dev-code-create <ayushkankale04@gmail.com>
2025-07-17 02:01:45 +0530

    Update Feature_Scaling&Normalization.py

--- Machine Learning/Feature_Scaling&Normalization.py
+++ Machine Learning/Feature_Scaling&Normalization.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 data = pd.DataFrame({
@@ -10,3 +11,9 @@ scaler = StandardScaler()
 scaled_data = scaler.fit_transform(data)
 
 print(pd.DataFrame(scaled_data,columns=["Age","Income"]))
+
+#Min-Max Normalization
+Scaler = MinMaxScaler()
+norm_data = Scaler.fit_transform(data)
+
+print(pd.DataFrame(norm_data, columns=["Age","Income"]))
