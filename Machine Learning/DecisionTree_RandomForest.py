4ad8bea6e51ae37128e38b9137dc6b818cdb8d92
dev-code-create <ayushkankale04@gmail.com>
2025-07-18 01:35:32 +0530

    Create DecisionTree_RandomForest.py

---
+++ Machine Learning/DecisionTree_RandomForest.py
@@ -0,0 +1,24 @@
+#Decision tree =>A Decision Tree is like a flowchart that asks a series of yes/no questions to make a decision.
+
+#Random_Forest => A Random Forest = many decision trees combined together to make a stronger prediction.
+
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+#Load Dataset
+iris = load_iris()
+X = iris.data
+Y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+dt = DecisionTreeClassifier()
+
+dt.fit(X_train, y_train)
+
+dt_pred = dt.predict(X_test)
+
+print("Decision Tree Accuracy:", accuracy_score(y_test, dt_pred))
