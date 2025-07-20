f1a3edd4bb9835722a421ad6351e3600ee2e1b33
dev-code-create <ayushkankale04@gmail.com>
2025-07-21 03:19:12 +0530

    Update titanic.py

--- Projects-mini/titanic.py
+++ Projects-mini/titanic.py
@@ -1,2 +1,45 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+#Step 1: Load & Explore the Data
 df = pd.read_csv("Titanic_dataset.csv")
+
+print(df.head())
+
+print(df.info())
+
+print(df.isnull().sum())
+
+# Step 2 : Data Cleaning
+
+df.drop('Cabin', axis=1 , inplace=True)
+
+df['Age'].fillna(df['Age'].median(), inplace=True)
+
+df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+
+df['Embarked'] = df['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+
+df.drop(['Name', 'Ticket', 'PassengerId'], axis=1, inplace=True)
+
+# Step 3 : Feature Selection + Train-Test Split
+
+X = df.drop('Survived' , axis = 1)
+Y = df['Survived']               # target label
+
+X_train , X_test , Y_train  ,Y_test = train_test_split(
+  X , Y , test_size=0.2 , random_state=42
+)
+
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train , Y_train)
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(Y_test, y_pred))
+print("\nConfusion Matrix:\n", confusion_matrix(Y_test, y_pred))
+print("\nClassification Report:\n", classification_report(Y_test, y_pred))
