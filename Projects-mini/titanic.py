6cb3ef48b9294b02ec292335cf6e266de628f6a1
dev-code-create <ayushkankale04@gmail.com>
2025-07-22 02:18:39 +0530

    Update titanic.py

--- Projects-mini/titanic.py
+++ Projects-mini/titanic.py
@@ -2,9 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 #Step 1: Load & Explore the Data
 df = pd.read_csv("Titanic_dataset.csv")
+le = LabelEncoder()
 
 print(df.head())
 
@@ -18,17 +21,33 @@ df.drop('Cabin', axis=1 , inplace=True)
 
 df['Age'].fillna(df['Age'].median(), inplace=True)
 
-df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+
+df['IsAlone'] = (df['FamilySize'] == 1).astype(int)
 
 df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
 
 df['Embarked'] = df['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
 
+scaler = StandardScaler()
+
+df[['Age', 'Fare', 'FamilySize']] = scaler.fit_transform(df[['Age', 'Fare', 'FamilySize']])
+
+df['Sex'] = le.fit_transform(df['Sex'])
+df['Embarked'] = le.fit_transform(df['Embarked'])
+
+df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+
+
+
 df.drop(['Name', 'Ticket', 'PassengerId'], axis=1, inplace=True)
+
+
+
 
 # Step 3 : Feature Selection + Train-Test Split
 
-X = df.drop('Survived' , axis = 1)
+X = df[['Pclass', 'Sex', 'Age', 'Fare', 'Embarked', 'FamilySize', 'IsAlone']]
 Y = df['Survived']               # target label
 
 X_train , X_test , Y_train  ,Y_test = train_test_split(
